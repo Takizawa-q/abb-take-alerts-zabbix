8f6a4564a7164a727687033da300725978b6c931
root <root@PC-123123>
2026-03-25 14:27:04 +0300

    add: check for zni is close or not

--- tools.py
+++ tools.py
@@ -1,10 +1,32 @@
 from datetime import date, datetime, timedelta
+import re
+import re
+import jira_api
 import time
-import telebot
 
 from aiogram import Bot
 
 from modules.log import logger
+
+
+def parse_zni_link(zni_text: str) -> str:
+    """
+    Извлекает CM-номер из текста zni_link и преобразует в markdown-ссылку.
+    Например: 'CM-10545 Сервисы: ...' -> '[CM-10545](https://team.akbars.ru/browse/CM-10545)'
+    """
+    if not zni_text:
+        return ""
+
+    # Ищем первое вхождение CM-номера (CM-XXXXX)
+    match = re.search(r'CM-\d+', zni_text)
+    if match:
+        cm_number = match.group(0)
+        cm = jira_api.jira.get_cm(cm_number)
+        resolution = (cm.raw.get('fields') or {}).get('resolution')
+        if resolution and resolution.get('description') == 'Отклонено':
+            return ""
+        return zni_text
+    return ""
 
 
 def take_tags(i):
@@ -43,13 +65,14 @@ def check_params(time_from, time_till):
         print(time_from)
     if not time_till:
         params = {
-            "time_from": time_from, #1682888400, # 1.05.2023
+            "time_from": time_from,  # 1682888400, # 1.05.2023
             # "eventids":  [740691464],
             # "time_till": time_till, #1688158800, # 1.07.2023
             "output": "extend",
             "select_acknowledges": "extend",
             "selectSuppressionData": "extend",  # select_suppression_data
-            "selectRelatedObject": ["triggerid", "description", "priority", "lastchange"],  #select_relatedObject
+            # select_relatedObject
+            "selectRelatedObject": ["triggerid", "description", "priority", "lastchange"],
             "severities": [4, 5],
             "selectHosts": "extend",
             # "filterCustomTime": "extend",
@@ -61,11 +84,12 @@ def check_params(time_from, time_till):
             "time_from": time_from,  # 1682888400, # 1.05.2023
             # "eventids": 385287521,
             # "eventids": [485760166, 485744631],
-            "time_till": time_till, #1688158800, # 1.07.2023
+            "time_till": time_till,  # 1688158800, # 1.07.2023
             "output": "extend",
             "select_acknowledges": "extend",
-            "selectSuppressionData": "extend",   #select_suppression_data
-            "selectRelatedObject": ["triggerid", "description", "priority", "lastchange"],  #
+            "selectSuppressionData": "extend",  # select_suppression_data
+            #
+            "selectRelatedObject": ["triggerid", "description", "priority", "lastchange"],
             "severities": [4, 5],
             "selectHosts": "extend",
             # "filterCustomTime": "extend"
@@ -73,16 +97,15 @@ def check_params(time_from, time_till):
     return params
 
 
-async def send_message_tg(problem_id, created, host, problem): #(
+async def send_message_tg(problem_id, created, host, problem):  # (
     token_test_bot = '6884868674:AAHHRZPHYSC31CwZTT99_78zbyoZDyGxN5E'
     proxy_url = 'http://mcafee.abb-win.akbars.ru:8080'
     bot = Bot(token=token_test_bot, messageroxy=proxy_url)  #
     # bot = telebot.TeleBot(token_test_bot)
     text = f'ID:{problem_id}\n' \
-           f'Дата начала: {created}\n' \
-           f'Хост: {host}\n' \
-           f'Описаине: {problem}'
-    
+        f'Дата начала: {created}\n' \
+        f'Хост: {host}\n' \
+        f'Описаине: {problem}'
+
     await bot.send_message(512578806, text, parse_mode='HTML', )
     logger.info(f'Send message: {problem_id}')
-
